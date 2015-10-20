57df60ac74325b2959227a029a98567413b7a84a
viveksyngh <rahul.syngh@gmail.com>
2015-10-20 11:39:28 +0530

    Insert Data

--- db_create.py
+++ db_create.py
@@ -3,12 +3,15 @@ from project.model import BlogPost, User, Track
 
 #create the database and the db tablse
 
-db.create_all()
+#db.create_all()
 
 #insert
 
 #db.session.add(BlogPost("Good", "I\' m good."))
 #db.session.add(BlogPost("Well", "I\' m Well. "))
+
+db.session.add(Track("Songs.PK_01_-_Jackpot_-_Kabhi_Jo_Baadal_Barse.mp3", "project\home\uploads\Songs.PK_01_-_Jackpot_-_Kabhi_Jo_Baadal_Barse.mp3", 7, 1, 1))
+#db.session.add(Track("Well", "I\' m Well. "))
 
 
 #commit
